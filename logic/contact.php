417fd620141fa8eebdc1ff0fe5dbd86271cf97cf
oluwamayor <rtlinksolutions@gmail.com>
2021-03-29 17:08:49 +0100

    contact page

--- logic/contact.php
+++ logic/contact.php
@@ -1,9 +1,7 @@
 <?php
 
 	use PHPMailer\PHPMailer\PHPMailer;
-    require_once 'PHPMailer/Exception.php';
-    require_once 'PHPMailer/PHPMailer.php';
-    require_once 'PHPMailer/SMTP.php';
+    
 
      $mail = new PHPMailer(true); 
 
@@ -61,13 +59,16 @@
 		}
 	
         else{
+            include_once "PHPMailer/Exception.php";
+            include_once "PHPMailer/PHPMailer.php";
+            include_once "PHPMailer/SMTP.php";
             $mail->isSMTP();
-            $mail->Host = 'smtp.gmail.com';
+            $mail->Host = "smtp.gmail.com";
             $mail->SMTPAuth = true;
-            $mail->Username = 'demot342@gmail.com'; // Gmail address which you want to use as SMTP server
-            $mail->Password = 'demot_$$78'; // Gmail address Password
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTSSL;
-            $mail->Port = '587';
+            $mail->Username = "demot342@gmail.com"; // Gmail address which you want to use as SMTP server
+            $mail->Password = "demot_$$78"; // Gmail address Password
+            $mail->SMTPSecure = "ssl";
+            $mail->Port = "587";
         
             $mail->setFrom('demot342@gmail.com'); // Gmail address which you used as SMTP server
             $mail->addAddress('princemayor96@gmail.com'); // Email address where you want to receive emails (you can use any of your gmail address including the gmail address which you used as SMTP server)
