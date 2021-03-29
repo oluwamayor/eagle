0d4273a9d41c2c156220c99686571f9ce6fbbc5b
oluwamayor <rtlinksolutions@gmail.com>
2021-03-29 19:45:05 +0100

    contact page

--- logic/send.php
+++ logic/send.php
@@ -45,39 +45,19 @@
         # Send the email.
         $success = mail($mail_to, $subject, $content, $headers);
         if ($success) {
-            # Set a 200 (okay) response code.
-            http_response_code(200);
-            
-            echo "Thank You! Your message has been sent.";
-          
-        } else {
-            # Set a 500 (internal server error) response code.
-            http_response_code(500);
-            echo "
-            <script type=\"text/javascript\">
-            Swal.fire({
-              icon: 'error',
-              title: 'Oops...',
-              text: 'Something went wrong'
-              
-            })
-            </script>
-            ";
-        }
+          # Set a 200 (okay) response code.
+          http_response_code(200);
+          echo "Thank You! Your message has been sent.";
+      } else {
+          # Set a 500 (internal server error) response code.
+          http_response_code(500);
+          echo "Oops! Something went wrong, we couldn't send your message.";
+      }
 
-    } else {
-        # Not a POST request, set a 403 (forbidden) response code.
-        http_response_code(403);
-        echo "
-        <script type=\"text/javascript\">
-        Swal.fire({
-          icon: 'error',
-          title: 'Oops...',
-          text: 'There was a problem with your submission'
-          
-        })
-        </script>
-        ";
-    }
+  } else {
+      # Not a POST request, set a 403 (forbidden) response code.
+      http_response_code(403);
+      echo "There was a problem with your submission, please try again.";
+  }
 
 ?>
